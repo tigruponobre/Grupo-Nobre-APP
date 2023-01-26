f5956c0eb3446fd388ae18ea73a86e9897cbd151
Gustavo Pasqua de Queiroz <dev.gustavo@outlook.com>
2023-01-26 19:07:57 -0300

    Adicionada funcionalidade de escolher qual conteúdo visualizar. - Adicionar efeitos visuais em breve

---
+++ public/scripts/netlify/netlify.js
@@ -0,0 +1,39 @@
+//Get elements
+let about = document.querySelector('#about')
+let construction = document.querySelector('#construction')
+let application = document.querySelector('#application')
+let containers = Array.from(document.querySelectorAll('.container'))
+
+let choicesDiv = document.querySelector('.choices')
+let choices = document.querySelectorAll('.choice')
+
+let checkbox = document.querySelector('#sandwich-check')
+let x = document.querySelector('#sandwich')
+
+choices[0].addEventListener('click', ()=>{
+    choicesDiv.classList.toggle('disappears')
+    about.classList.toggle('disappears')
+    checkbox.checked = false
+})
+choices[1].addEventListener('click', ()=>{
+    choicesDiv.classList.toggle('disappears')
+    construction.classList.toggle('disappears')
+    checkbox.checked = false
+})
+choices[2].addEventListener('click', ()=>{
+    choicesDiv.classList.toggle('disappears')
+    application.classList.toggle('disappears')
+    checkbox.checked = false
+})
+
+x.addEventListener('click', ()=>{
+    if(!checkbox.checked){
+        for(let index in containers){
+            if(!containers[index].classList.contains('disappears')){
+                containers[index].classList.add('disappears')
+            }
+        }
+        // if()
+        choicesDiv.classList.toggle('disappears')
+    }
+})
