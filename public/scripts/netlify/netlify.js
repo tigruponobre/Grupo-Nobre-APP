b2d60af384bf3166396de2a4b5deb2f383b04a24
Gustavo Pasqua de Queiroz <dev.gustavo@outlook.com>
2023-01-27 12:58:21 -0300

    Adicionado botão de voltar ao menu principal

--- public/scripts/netlify/netlify.js
+++ public/scripts/netlify/netlify.js
@@ -3,12 +3,17 @@ let about = document.querySelector('#about')
 let construction = document.querySelector('#construction')
 let application = document.querySelector('#application')
 let containers = Array.from(document.querySelectorAll('.container'))
+let url = sessionStorage.url
 
 let choicesDiv = document.querySelector('.choices')
 let choices = document.querySelectorAll('.choice')
 
 let checkbox = document.querySelector('#sandwich-check')
 let x = document.querySelector('#sandwich')
+
+let buttonDiv = document.querySelector('.back')
+let backButton = document.querySelector('.backButton')
+let label = document.querySelector('.backLabel')
 
 choices[0].addEventListener('click', ()=>{
     choicesDiv.classList.toggle('disappears')
@@ -37,4 +42,18 @@ x.addEventListener('click', ()=>{
             choicesDiv.classList.remove('disappears')
         }
     }
+})
+
+backButton.addEventListener('click', ()=> window.location.href = url)
+
+
+backButton.addEventListener('mouseover', ()=>{
+    buttonDiv.style.transform = 'translateY(-10px)'
+    label.style.transform = 'translateY(-5px)'
+    label.style.opacity = '1'
+})
+backButton.addEventListener('mouseout', ()=>{
+    buttonDiv.style.transform = 'translateY(0)'
+    label.style.transform = 'translateY(5px)'
+    label.style.opacity = '0'
 })
