ca7b71710959089061cfbf0ffb117fb36e15cd11
Gustavo Pasqua de Queiroz <dev.gustavo@outlook.com>
2023-02-23 17:21:38 -0300

    Adicionado div main para cobrir espaço em branco quando usuário acessa a página - DIV de boas-vindas

--- public/scripts/administrative/administrative.js
+++ public/scripts/administrative/administrative.js
@@ -1,8 +1,15 @@
+let main = document.getElementsByTagName('main')[0]
+
 let menuItems = Array.from(document.querySelectorAll('#menu li'))
 let circles = Array.from(document.querySelectorAll('#circles li'))
 
+let documentations = document.getElementById('docs')
+
 for (let index in menuItems){
     menuItems[index].addEventListener('click', (event)=>{
+        //Remove main
+        main.style.display = 'none';
+
         for (let newIndex in menuItems){
             menuItems[newIndex].classList.remove('active')
             circles[newIndex].classList.remove('active')
@@ -10,6 +17,7 @@ for (let index in menuItems){
         event.target.classList.add('active')
         if(event.target.textContent == 'DOCUMENTAÇÕES'){
             circles[0].classList.add('active')
+            documentations.style.display = 'block'
         }
         else if(event.target.textContent == 'USUÁRIOS'){
             circles[1].classList.add('active')
