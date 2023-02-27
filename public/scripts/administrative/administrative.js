dc4c6f3b55027a60d985ae3228daa0bff6db7cda
Gustavo Pasqua de Queiroz <dev.gustavo@outlook.com>
2023-02-27 14:52:38 -0300

    Criada a função de pesquisa de usuário e ajustado aparecimento da box ao click no botão do menu

--- public/scripts/administrative/administrative.js
+++ public/scripts/administrative/administrative.js
@@ -3,6 +3,7 @@ let main = document.getElementsByTagName('main')[0]
 let menuItems = Array.from(document.querySelectorAll('#menu li'))
 let circles = Array.from(document.querySelectorAll('#circles li'))
 let documentations = document.getElementById('docs')
+let consultUsers = document.getElementById('consultUsers')
 
 //SELECT MENU BUTTON
 for (let index in menuItems){
@@ -17,13 +18,18 @@ for (let index in menuItems){
         event.target.classList.add('active')
         if(event.target.textContent == 'DOCUMENTAÇÕES'){
             circles[0].classList.add('active')
+            consultUsers.style.display = 'none'
             documentations.style.display = 'block'
         }
         else if(event.target.textContent == 'USUÁRIOS'){
             circles[1].classList.add('active')
+            documentations.style.display = 'none'
+            consultUsers.style.display = 'block'
         }
         else if(event.target.textContent == 'CADASTRO'){
             circles[2].classList.add('active')
+            consultUsers.style.display = 'none'
+            documentations.style.display = 'none'
         }
     })
 }
@@ -42,3 +48,20 @@ async function getUsers(){
     })
 }
 getUsers()
+
+
+//Search users
+let search = document.getElementById('search')
+search.addEventListener('keyup', (event)=>{
+    event.preventDefault()
+    let getUsersLi = Array.from(document.querySelectorAll('#users ul li'))
+    let searchValue = search.value
+    for (let user of getUsersLi){
+        user.classList.add('userDisappears')
+    }
+    for (let user of getUsersLi){
+        if(user.textContent.toLowerCase().match(searchValue.toLowerCase())){
+            user.classList.remove('userDisappears')
+        }
+    }
+})
