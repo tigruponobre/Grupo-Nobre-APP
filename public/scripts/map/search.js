ba0148fc2a2a9fd51bca11b79d64eb8388983fd6
Gustavo Pasqua de Queiroz <dev.gustavo@outlook.com>
2023-03-15 16:26:10 -0300

    Ajustada página teste de mapa

--- public/scripts/map/search.js
+++ public/scripts/map/search.js
@@ -1,27 +1,12 @@
-//Give all selects the same eventListener
-[document.getElementById('curso'), document.getElementById('turno'), document.getElementById('dia')].forEach(element => {
-    element.addEventListener('change', ()=>{
-        let checkSelects = document.getElementById('curso').value != 'curso' && document.getElementById('turno').value != 'turno' && document.getElementById('dia').value != 'dia'
-        if(checkSelects && window.innerWidth > 680) {
-            searchMapDesktop(document.getElementById('curso').value, document.getElementById('turno').value, document.getElementById('dia').value)
-        }else if(checkSelects && window.innerWidth <= 680){
-            searchMapMobile(document.getElementById('curso').value, document.getElementById('turno').value, document.getElementById('dia').value)
-        }
-    })
-});
-
-//url
-const url = document.URL.split('/').splice('0',3).join('/')
-
-async function searchMapDesktop(curso, turno, dia){
+async function searchMapDesktop(curso, turno, dia) {
     //Reset table
-    document.getElementById('myTable').innerHTML = 
-    `<tr>
+    document.getElementById('myTable').innerHTML =
+        `<tr>
     <th>CURSO</th>
     <th>TURNO</th>
     <th>DIA DA SEMANA</th>
-    <th>TURMA</th>
     <th>DISCIPLINA</th>
+    <th>TURMA</th>
     <th>PROFESSOR(A)</th>
     <th>SALA</th>
     <th>MÓDULO</th>
@@ -33,7 +18,7 @@ async function searchMapDesktop(curso, turno, dia){
     let information = ''
 
     //Fetch
-    const response = await fetch(url + '/.netlify/functions/searchmap', {
+    const response = await fetch('https://home.gruponobre.edu.br/.netlify/functions/searchmap', {
         method: 'post',
         body: JSON.stringify({
             curso: curso,
@@ -42,25 +27,26 @@ async function searchMapDesktop(curso, turno, dia){
         })
     })
     const data = await response.json()
-    if (response.status == 200){
+
+    if (response.status == 200) {
         information = await data.information
-        if(!information){
+        if (!information) {
             document.getElementById('myTable').innerHTML =
-            `<tr>
+                `<tr>
             <th>NÃO ENCONTRADO</th>
             </tr>`
             return
         }
-    }else{
+    } else {
         document.getElementById('myTable').innerHTML =
-        `<tr>
+            `<tr>
         <th>NÃO ENCONTRADO</th>
         </tr>`
         return
     }
 
     //Updating table
-    for (let elem of Array.from(Object.keys(information))){
+    for (let elem of Array.from(Object.keys(information))) {
         //New Row
         let newRow = document.createElement('tr')
 
@@ -80,13 +66,13 @@ async function searchMapDesktop(curso, turno, dia){
         newDia.innerText = document.getElementById('dia').value
         newRow.appendChild(newDia)
 
-        //Disciplina from response
-        let newDisciplina = document.createElement('td')
-        newDisciplina.innerText = elem
-        newRow.appendChild(newDisciplina)
+        //Turma from response
+        let newTurma = document.createElement('td')
+        newTurma.innerText = elem
+        newRow.appendChild(newTurma)
 
         //Information in JSON from the request
-        for (let key of Array.from(Object.keys(information[elem]))){
+        for (let key of Array.from(Object.keys(information[elem]))) {
             let newData = document.createElement('td')
             newData.innerText = information[elem][key]
             newRow.appendChild(newData)
@@ -97,12 +83,12 @@ async function searchMapDesktop(curso, turno, dia){
     }
 }
 
-async function searchMapMobile(curso, turno, dia){
+async function searchMapMobile(curso, turno, dia) {
     //Declearing information variable
     let information = ''
 
     //Fetch
-    const response = await fetch(url + '/.netlify/functions/searchmap', {
+    const response = await fetch('https://home.gruponobre.edu.br/.netlify/functions/searchmap', {
         method: 'post',
         body: JSON.stringify({
             curso: curso,
@@ -111,12 +97,19 @@ async function searchMapMobile(curso, turno, dia){
         })
     })
     const data = await response.json()
-    if (response.status == 200){
+    if (response.status == 200) {
         information = await data.information
         document.getElementById('tables').innerHTML = ''
-    }else{
+        if (!information) {
+            document.getElementById('myTable').innerHTML =
+                `<tr>
+            <th>NÃO ENCONTRADO</th>
+            </tr>`
+            return
+        }
+    } else {
         document.getElementById('tables').innerHTML =
-        `<table>
+            `<table>
         <tr>
         <th>NÃO ENCONTRADO</th>
         </tr>
@@ -124,7 +117,7 @@ async function searchMapMobile(curso, turno, dia){
         return
     }
 
-    for (let elem of Array.from(Object.keys(information))){
+    for (let elem of Array.from(Object.keys(information))) {
         //Getting info
         let newCurso = document.getElementById('curso').value
         let newTurno = document.getElementById('turno').value
@@ -139,13 +132,13 @@ async function searchMapMobile(curso, turno, dia){
 
         let newTable = document.createElement('table')
         newTable.setAttribute('class', 'newTable')
-        newTable.innerHTML = 
-        `<tr>
+        newTable.innerHTML =
+            `<tr>
         <th>CURSO: ${newCurso}</th>
         <th>TURNO: ${newTurno}</th>
         <th>DIA DA SEMANA: ${newDia}</th>
-        <th>TURMA: ${newTurma}</th>
         <th>DISCIPLINA: ${newDisciplina}</th>
+        <th>TURMA: ${newTurma}</th>
         <th>PROFESSOR(A): ${newProfessor}</th>
         <th>SALA: ${newSala}</th>
         <th>MÓDULO: ${newModulo}</th>
