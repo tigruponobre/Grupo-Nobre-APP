b1a099c927ca64f330a854057c25304b0f928de1
Gustavo Pasqua de Queiroz <dev.gustavo@outlook.com>
2023-03-30 17:57:03 -0300

    Ajustado para div de update password aparecer ao clickar nesta opção da tela de login

--- public/scripts/home/updatePassword.js
+++ public/scripts/home/updatePassword.js
@@ -1,4 +1,9 @@
 //Declearing variables
-const fade = document.querySelector('.fade')
-const loginLogin = Array.from(document.querySelectorAll('.update_password .inputs input'))
+const call_update_password = document.getElementById('call_update_password')
+let updateInputs = Array.from(document.querySelectorAll('.update_password .inputs input'))
+let update_password_div = document.querySelector('.update_password')
 
+call_update_password.addEventListener('click', ()=>{
+    loginDiv.classList.toggle('unhide')
+    update_password_div.classList.toggle('unhide')
+})
