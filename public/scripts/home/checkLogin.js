7459023601ef8048db4734d52e536e17e3b20556
Gustavo Pasqua de Queiroz <dev.gustavo@outlook.com>
2023-04-04 11:25:26 -0300

    Ajuste de bug ao clickar no painel administrativo após deslogar

--- public/scripts/home/checkLogin.js
+++ public/scripts/home/checkLogin.js
@@ -14,6 +14,17 @@ function checkLogin(){
         document.getElementById('greeting').textContent = 'Bem-vindo ao'
         login_logout.textContent = 'Login'
     }
+
+    //Admin Panel
+    let adminButton = document.getElementById('admin')
+    let permissions = sessionStorage.getItem('permissions')
+    if(permissions == 'room-map'){
+        adminButton.addEventListener('click', ()=> window.location.href = url + '/pages/mapa-admin')
+    }else if(permissions == 'faq'){
+        adminButton.addEventListener('click', ()=> window.location.href = url + '/pages/base-de-conhecimento')
+    }else{
+        adminButton.addEventListener('click', ()=> window.location.href = url + '/pages/administrativo')
+    }
 }
 checkLogin()
 
