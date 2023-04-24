e379816e16e2de8f2b5885f34d6c6c20dba0bce2
Gustavo Pasqua de Queiroz <dev.gustavo@outlook.com>
2023-04-24 18:06:49 -0300

    Iniciado o processo de requisição para trazer perguntas respondidas do tema escolhido - comentários dizem como prosseguir

--- public/scripts/faq/generateTheme.js
+++ public/scripts/faq/generateTheme.js
@@ -23,5 +23,27 @@ async function generateTheme(event){
 
     const data = await response.json()
 
-    console.log(data)
+    if(response.status != 200){
+        window.alert(data.msg)
+    }else{
+        const questions = data.content
+        questions.forEach( element => {
+            const showQuestion = document.createElement('div')
+            showQuestion.setAttribute('class', 'question')
+            showQuestion.innerHTML = 
+            `<h5 class="questionTitle">${element.title}<img onclick="showResponse(event)"
+            src="../img/icons/black_left_arrow.png" alt="arrow-right" width="25"></h5>
+            <div class="unef response">
+                <h6>UNEF</h6>
+                <p>Qualquer dúvida acadêmica, direcionado a coordenação e professores. Aluno EAD não resolve
+                problemas acadêmicas no NAC - UNEF.</p>
+            </div>
+            <div class="unifan response">
+                <h6>UNIFAN</h6>
+                <p>Alunos com dificuldade passados o contato geral da huggy (2102-9100) e pedimos para falar com o NAC.</p>
+            </div>`
+
+            //ANTES DE FAZER O QUE ESTAVA FAZENDO ACIMA É NECESSÁRIO VERIFICAR QUAIS CAMPOS TEM CONTÉUDO, UNEF - UNINFAN - UNEF EAD - UNIFAN EAD E CRIAR DIVS PARA CADA QUANDO HOUVER CONTEÚDO, CONCATENAR COMO STRING E SÓ DEPOIS EXECUTAR O APPEND
+        })
+    }
 }
