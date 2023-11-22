9bb6f3ad16fd2e02acfcfb71368075c7538582c5
Gabriel Queiroz <gabrielsq@yahoo.com.br>
2023-11-22 10:27:18 -0300

    Alteração index

--- functions/create_map_unef.js
+++ functions/create_map_unef.js
@@ -8,15 +8,18 @@ const db_pass = process.env.DB_TI_PASSWORD
 const cluster = process.env.DB_TI_CLUSTER
 const db_name = process.env.DB_TI_NAME
 
-exports.handler = async function (event, context){
+exports.handler = async function (event, context) {
     //Connection with MongoDB Atlas
     let connection = await mongoose.connect(`mongodb+srv://${db_user}:${db_pass}@${cluster}.qk8butq.mongodb.net/${db_name}?retryWrites=true&w=majority`)
 
+    // let connection = await mongoose.connect(`mongodb+srv://${db_user}:${db_pass}@${cluster}.qk8butq.mongodb.net/teste?retryWrites=true&w=majority`)
+
+
     //Check if connetion succeeded
-    if(connection){
+    if (connection) {
         console.log('Connection successful!')
-    }else{
-        return{
+    } else {
+        return {
             statusCode: 500,
             body: JSON.stringify({
                 resposta: "Connection failed."
@@ -27,9 +30,9 @@ exports.handler = async function (event, context){
     // Destructuring variabels
     const eventBody = await JSON.parse(event.body)
     let { CURSO, MODALIDADE, MATUTINO, VESPERTINO, NOTURNO } = eventBody
-    
+
     // Discipline object if all shifts needs to be registered
-    if(MATUTINO && VESPERTINO && NOTURNO){
+    if (MATUTINO && VESPERTINO && NOTURNO) {
         let newMap = {
             CURSO,
             MODALIDADE,
@@ -41,7 +44,7 @@ exports.handler = async function (event, context){
         // Create discipline
         try {
             await Map.create(newMap)
-            return{
+            return {
                 statusCode: 200,
                 body: JSON.stringify({
                     resposta: 'Map registered'
@@ -52,47 +55,47 @@ exports.handler = async function (event, context){
                 statusCode: 500,
                 body: JSON.stringify({
                     resposta: 'It was not possible to registrate this map'
-                    })
-                }
+                })
             }
+        }
     }
     // Discipline object if MATUTINO and NOTURNO needs to be registered
-    else if (MATUTINO && NOTURNO){
+    else if (MATUTINO && NOTURNO) {
         let newMap = {
             CURSO,
             MODALIDADE,
             MATUTINO,
             NOTURNO
         }
-    // Create discipline
-    try {
-        await Map.create(newMap)
-        return{
-            statusCode: 200,
-            body: JSON.stringify({
-                resposta: 'Map registered'
-            })
-        }
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                resposta: 'It was not possible to registrate this map'
+        // Create discipline
+        try {
+            await Map.create(newMap)
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    resposta: 'Map registered'
+                })
+            }
+        } catch (error) {
+            return {
+                statusCode: 500,
+                body: JSON.stringify({
+                    resposta: 'It was not possible to registrate this map'
                 })
             }
         }
     }
     // Discipline object if only MATUTINO needs to be registered
-    else if (MATUTINO){
+    else if (MATUTINO) {
         let newMap = {
-                CURSO,
-                MODALIDADE,
-                MATUTINO
+            CURSO,
+            MODALIDADE,
+            MATUTINO
         }
         // Create discipline
         try {
             await Map.create(newMap)
-            return{
+            return {
                 statusCode: 200,
                 body: JSON.stringify({
                     resposta: 'Map registered'
@@ -108,26 +111,26 @@ exports.handler = async function (event, context){
         }
     }
     // Discipline object if only NOTURNO needs to be registered
-    else if (NOTURNO){
+    else if (NOTURNO) {
         let newMap = {
             CURSO,
             MODALIDADE,
             NOTURNO
-    }
-    // Create Discipline
-    try {
-        await Map.create(newMap)
-        return{
-            statusCode: 200,
-            body: JSON.stringify({
-                resposta: 'Map registered'
-            })
         }
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                resposta: 'It was not possible to registrate this map'
+        // Create Discipline
+        try {
+            await Map.create(newMap)
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    resposta: 'Map registered'
+                })
+            }
+        } catch (error) {
+            return {
+                statusCode: 500,
+                body: JSON.stringify({
+                    resposta: 'It was not possible to registrate this map'
                 })
             }
         }
