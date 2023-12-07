691eb44e50f43244ccb9ae600a39c3dc43be656b
Gabriel Queiroz <gabrielsq@yahoo.com.br>
2023-12-07 11:14:40 -0300

    api chatgpt

--- functions/api-key.js
+++ functions/api-key.js
@@ -1,1 +1,30 @@
 // API QUE CONTÊM A CHAVE DA API DO CHATGPT, UTILIZADO NO PROJETO DA INTELIGÊNCIA ARTIFICIAL
+
+require('dotenv').config();
+
+exports.handler = async function (event, context) {
+    // Configurando os cabeçalhos CORS
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+    };
+
+    try {
+        // Obtendo a chave de API da variável de ambiente
+        const apiKey = process.env.OPENAI_API_KEY;
+
+        // Retornando uma resposta JSON com a chave de API
+        return {
+            statusCode: 200,
+            headers,
+            body: JSON.stringify({ apiKey }),
+        };
+    } catch (error) {
+        // Em caso de erro, retornar uma resposta de erro
+        return {
+            statusCode: 500,
+            headers,
+            body: JSON.stringify({ error: 'Erro interno do servidor' }),
+        };
+    }
+};
