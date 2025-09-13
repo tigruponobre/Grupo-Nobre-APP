85430ea7052107f6a03ca7df339af4838aa89f1b
Grupo Nobre <88802232+tigruponobre@users.noreply.github.com>
2025-09-13 12:06:28 -0300

    atualização da bibli_unef

--- functions/bibli_unef.js
+++ functions/bibli_unef.js
@@ -6,153 +6,118 @@ require('dotenv').config()
 const secret = process.env.BIBLI_UNEF_SECRET
 
 exports.handler = async function (event, context){
-    // Headers CORS completos para todas as respostas
-    const corsHeaders = {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
-    };
+    //Get student information from body requistion
+    let eventBody = JSON.parse(event.body)
+    const firstName = eventBody.firstName
+    const lastName = eventBody.lastName
+    const ra = eventBody.ra
 
-    // Handle preflight requests (OPTIONS)
-    if (event.httpMethod === 'OPTIONS') {
+    //Consume library API
+    let response = await axios('https://digitallibrary.zbra.com.br/DigitalLibraryIntegrationService/AuthenticatedUrl', {
+        method: 'post',
+        headers: {
+            'Content-type': 'text/xml',
+            "X-DigitalLibraryIntegration-API-Key": `${secret}` //CHAVE DA UNEF,
+        },
+        data: (`<?xml version="1.0" encoding="utf-8"?>
+        <CreateAuthenticatedUrlRequest
+        xmlns="http://dli.zbra.com.br"
+        xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <FirstName>${firstName}</FirstName>
+        <LastName>${lastName}</LastName>
+        <Email>${ra}</Email>
+        <CourseId xsi:nil="true"/>
+        <Tag xsi:nil="true"/>
+        <Isbn xsi:nil="true"/>
+        </CreateAuthenticatedUrlRequest>
+        `)
+    })
+    
+    //Get response data
+    let data = await response.data
+
+    //Success -> Return authenticated URL
+    if(data.Success == true){
         return {
             statusCode: 200,
-            headers: corsHeaders,
-            body: ''
-        };
+            headers: {
+                "Access-Control-Allow-Origin": "*"
+            },
+            body: JSON.stringify({
+                resposta: data.AuthenticatedUrl
+            })
+        }
+    }
+    
+    //Else -> Create user in library database
+    let createUser = await axios({
+        method: 'post',
+        url: 'https://digitallibrary.zbra.com.br/DigitalLibraryIntegrationService/CreatePreRegisterUser',
+        headers: {
+            'Content-type': 'text/xml',
+            "X-DigitalLibraryIntegration-API-Key": `${secret}` //UNEF SECRET KEY
+        },
+        data: (`<?xml version="1.0" encoding="utf-8"?>
+        <CreatePreRegisterUserRequest
+        xmlns="http://dli.zbra.com.br"
+        xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <FirstName>${firstName}</FirstName>
+        <LastName>${lastName}</LastName>
+        <UserName>${ra}</UserName>
+        </CreatePreRegisterUserRequest>
+        `)
+    })
+
+    //Error creating user
+    if(createUser.data.Success == false){
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                resposta: "Usuário NÃO cadastrado!"
+            })
+        }
     }
 
-    try {
-        //Get student information from body requistion
-        let eventBody = JSON.parse(event.body)
-        const firstName = eventBody.firstName
-        const lastName = eventBody.lastName
-        const ra = eventBody.ra
+    //Success creating user -> Return authenticated URL
+    response = await axios('https://digitallibrary.zbra.com.br/DigitalLibraryIntegrationService/AuthenticatedUrl', {
+        method: 'post',
+        headers: {
+            'Content-type': 'text/xml',
+            "X-DigitalLibraryIntegration-API-Key": `${secret}` //CHAVE DA UNEF
+        },
+        data: (`<?xml version="1.0" encoding="utf-8"?>
+        <CreateAuthenticatedUrlRequest
+        xmlns="http://dli.zbra.com.br"
+        xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <FirstName>${firstName}</FirstName>
+        <LastName>${lastName}</LastName>
+        <Email>${ra}</Email>
+        <CourseId xsi:nil="true"/>
+        <Tag xsi:nil="true"/>
+        <Isbn xsi:nil="true"/>
+        </CreateAuthenticatedUrlRequest>
+        `)
+    })
+    
+    //Get response data
+    data = await response.data
 
-        //Consume library API - Nova URL e headers
-        let response = await axios('https://integracao.dli.minhabiblioteca.com.br/DigitalLibraryIntegrationService/AuthenticatedUrl', {
-            method: 'post',
-            headers: {
-                'Host': 'integracao.dli.minhabiblioteca.com.br',
-                'Content-Type': 'application/xml; charset=utf-8',
-                'X-DigitalLibraryIntegration-API-Key': `${secret}` //CHAVE DA UNEF
-            },
-            data: (`<?xml version="1.0" encoding="utf-8"?>
-            <CreateAuthenticatedUrlRequest
-            xmlns="http://dli.zbra.com.br"
-            xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-            <FirstName>${firstName}</FirstName>
-            <LastName>${lastName}</LastName>
-            <Email>${ra}</Email>
-            <CourseId xsi:nil="true"/>
-            <Tag xsi:nil="true"/>
-            <Isbn xsi:nil="true"/>
-            </CreateAuthenticatedUrlRequest>
-            `)
-        })
-        
-        //Get response data
-        let data = await response.data
-
-        //Success -> Return authenticated URL
-        if(data.Success == true){
-            return {
-                statusCode: 200,
-                headers: corsHeaders,
-                body: JSON.stringify({
-                    resposta: data.AuthenticatedUrl
-                })
-            }
+    //Success
+    if(data.Success == true){
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                resposta: data.AuthenticatedUrl
+            })
         }
-        
-        //Else -> Create user in library database
-        let createUser = await axios({
-            method: 'post',
-            url: 'https://integracao.dli.minhabiblioteca.com.br/DigitalLibraryIntegrationService/CreatePreRegisterUser',
-            headers: {
-                'Host': 'integracao.dli.minhabiblioteca.com.br',
-                'Content-Type': 'application/xml; charset=utf-8',
-                'X-DigitalLibraryIntegration-API-Key': `${secret}` //UNEF SECRET KEY
-            },
-            data: (`<?xml version="1.0" encoding="utf-8"?>
-            <CreatePreRegisterUserRequest
-            xmlns="http://dli.zbra.com.br"
-            xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-            <FirstName>${firstName}</FirstName>
-            <LastName>${lastName}</LastName>
-            <UserName>${ra}</UserName>
-            </CreatePreRegisterUserRequest>
-            `)
-        })
-
-        //Error creating user
-        if(createUser.data.Success == false){
-            return {
-                statusCode: 400,
-                headers: corsHeaders,
-                body: JSON.stringify({
-                    resposta: "Usuário NÃO cadastrado!",
-                    erro: createUser.data.Message || "Erro desconhecido"
-                })
-            }
-        }
-
-        //Success creating user -> Return authenticated URL
-        response = await axios('https://integracao.dli.minhabiblioteca.com.br/DigitalLibraryIntegrationService/AuthenticatedUrl', {
-            method: 'post',
-            headers: {
-                'Host': 'integracao.dli.minhabiblioteca.com.br',
-                'Content-Type': 'application/xml; charset=utf-8',
-                'X-DigitalLibraryIntegration-API-Key': `${secret}` //CHAVE DA UNEF
-            },
-            data: (`<?xml version="1.0" encoding="utf-8"?>
-            <CreateAuthenticatedUrlRequest
-            xmlns="http://dli.zbra.com.br"
-            xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-            <FirstName>${firstName}</FirstName>
-            <LastName>${lastName}</LastName>
-            <Email>${ra}</Email>
-            <CourseId xsi:nil="true"/>
-            <Tag xsi:nil="true"/>
-            <Isbn xsi:nil="true"/>
-            </CreateAuthenticatedUrlRequest>
-            `)
-        })
-        
-        //Get response data
-        data = await response.data
-
-        //Success
-        if(data.Success == true){
-            return {
-                statusCode: 200,
-                headers: corsHeaders,
-                body: JSON.stringify({
-                    resposta: data.AuthenticatedUrl
-                })
-            }
-        }else{
-            return {
-                statusCode: 500,
-                headers: corsHeaders,
-                body: JSON.stringify({
-                    resposta: "Erro de servidor!",
-                    erro: data.Message || "Erro desconhecido"
-                })
-            }
-        }
-
-    } catch (error) {
-        console.error('Erro na integração da biblioteca UNEF:', error);
+    }else{
         return {
             statusCode: 500,
-            headers: corsHeaders,
             body: JSON.stringify({
-                resposta: "Erro interno do servidor",
-                erro: error.message
+                resposta: "Erro de servidor!"
             })
         }
     }
